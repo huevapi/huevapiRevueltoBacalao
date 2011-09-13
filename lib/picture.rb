a5d992f47f395c9065a02c31dc3b10ea61f79435
Juan Ignacio Pumarino <jipumarino@gmail.com>
2011-09-12 22:19:57 -0300

    Método para obtener la URL de una imagen twitpic a partir de la URL de la página

--- lib/picture.rb
+++ lib/picture.rb
@@ -1,4 +1,6 @@
+require 'open-uri'
 require 'mini_magick'
+require 'nokogiri'
 
 class Picture
 
@@ -6,7 +8,15 @@ class Picture
     begin
       MiniMagick::Image.open(url).identify.split(" ")[2].split("x")
     rescue
-      puts "Fallo al obtener la imagen"
+      puts "Error al obtener la imagen"
+    end
+  end
+
+  def self.get_image_url(page_url)
+    begin
+      Nokogiri::HTML(open(page_url)).at_css("#photo-display")['src']
+    rescue
+      puts "Error al descargar HTML de página de imagen"
     end
   end
 
