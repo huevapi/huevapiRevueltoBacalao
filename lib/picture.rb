7366a45ff8c1f2d41c058c37fdac0494cfccaf11
Juan Ignacio Pumarino <jipumarino@gmail.com>
2011-09-12 21:54:00 -0300

    Capturar excepción (if internet_sucks)

--- lib/picture.rb
+++ lib/picture.rb
@@ -3,7 +3,11 @@ require 'mini_magick'
 class Picture
 
   def self.get_size(url)
-    MiniMagick::Image.open(url).identify.split(" ")[2].split("x")
+    begin
+      MiniMagick::Image.open(url).identify.split(" ")[2].split("x")
+    rescue
+      puts "Fallo al obtener la imagen"
+    end
   end
 
 end
