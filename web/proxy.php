bdfd4e5d1fca84bdebc76f256431a9fcbc29ecad
Bertrand Fan <bertrand@fan.net>
2011-09-13 01:20:02 -0300

    Frontend using bin packing algorithm displaying in canvas

---
+++ web/proxy.php
@@ -0,0 +1,7 @@
+<?php
+
+	echo file_get_contents('http://192.168.100.100:3000/images.json');
+
+#	echo '{"images":[{"width":123,"height":123,"url":"http://s1.proxy04.twitpic.com/photos/large/396558902.jpg","link_url":"http://twitter.com/#!/27bslash6/status/103841853576712192","weight":3}]}';
+
+?>
